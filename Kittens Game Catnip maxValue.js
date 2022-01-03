502ab8b540abd92f249e1f46bd67cfa04d61409b
Amaury <amaury.data@gmail.com>
2022-01-03 21:16:41 +0800

    Update and rename test.js to Kittens Game Catnip maxValue.js

---
+++ Kittens Game Catnip maxValue.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Kittens Game Catnip maxValue
+// @namespace    https://github.com/Amaury-GitHub/kittensgame
+// @version      1.0
+// @description  猫国建设者,已解锁的资源自动锁定最大值
+// @author       Amaury
+// @include     *bloodrizer.ru/games/kittens/*
+// @include     file:///*kitten-game*
+// @include     *kittensgame.com/web/*
+// @include     *kittensgame.com/beta/*
+// @include     *kittensgame.com/alpha/*
+// @match        https://likexia.gitee.io/cat-zh/
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+function maxValue ()
+{
+    game.resPool.resourceMap.catnip.value = game.resPool.resourceMap.catnip.maxValue
+}
+
+setInterval(maxValue,1000);
+
+})()
