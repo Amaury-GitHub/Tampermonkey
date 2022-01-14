246c756d06d123f06c7ccb708f64f4d8a4333171
Amaury <amaury.data@gmail.com>
2022-01-14 23:21:48 +0800

    Update Kittens Game maxValue.user.js

--- Kittens Game maxValue.user.js
+++ Kittens Game maxValue.user.js
@@ -2,7 +2,7 @@
 // @name         Kittens Game maxValue
 // @namespace    https://github.com/Amaury-GitHub/kittensgame
 // @version      1.0
-// @description  猫国建设者,锁定猫薄荷的最大值
+// @description  猫国建设者,解放小猫
 // @author       Amaury
 // @include     *bloodrizer.ru/games/kittens/*
 // @include     file:///*kitten-game*
@@ -16,8 +16,37 @@
 (function() {
     'use strict';
 
+    //伐木工-木材
+    setInterval(function() {
+        game.resPool.resourceMap.wood.value = game.resPool.resourceMap.wood.maxValue
+    }, 1000)
+    //农民-猫薄荷
     setInterval(function() {
         game.resPool.resourceMap.catnip.value = game.resPool.resourceMap.catnip.maxValue
+    }, 1000)
+    //学者-科学
+    setInterval(function() {
+        game.resPool.resourceMap.science.value = game.resPool.resourceMap.science.maxValue
+    }, 1000)
+    //猎人-喵力
+    setInterval(function() {
+        game.resPool.resourceMap.manpower.value = game.resPool.resourceMap.manpower.maxValue
+    }, 1000)
+    //矿工-矿物
+    setInterval(function() {
+        game.resPool.resourceMap.minerals.value = game.resPool.resourceMap.minerals.maxValue
+    }, 1000)
+    //牧师-信仰
+    setInterval(function() {
+        game.resPool.resourceMap.faith.value = game.resPool.resourceMap.faith.maxValue
+    }, 1000)
+    //地质学家-煤
+    setInterval(function() {
+        game.resPool.resourceMap.coal.value = game.resPool.resourceMap.coal.maxValue
+    }, 1000)
+    //地质学家-黄金
+    setInterval(function() {
+        game.resPool.resourceMap.gold.value = game.resPool.resourceMap.gold.maxValue
     }, 1000)
 
 })();
