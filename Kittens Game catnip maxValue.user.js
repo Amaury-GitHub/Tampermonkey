ba970b1310528faf8b49ab17c91f59134ed92f49
Amaury <amaury.data@gmail.com>
2022-01-19 22:38:26 +0800

    Add files via upload

---
+++ Kittens Game catnip maxValue.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Kittens Game maxValue
+// @namespace    https://github.com/Amaury-GitHub/kittensgame
+// @version      1.0
+// @description  猫国建设者,解放小猫
+// @author       Amaury
+// @include     *bloodrizer.ru/games/kittens/*
+// @include     file:///*kitten-game*
+// @include     *kittensgame.com/web/*
+// @include     *kittensgame.com/beta/*
+// @include     *kittensgame.com/alpha/*
+// @match        https://likexia.gitee.io/cat-zh/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    setInterval(function() {
+        game.resPool.resourceMap.catnip.value = game.resPool.resourceMap.catnip.maxValue
+    }, 1000)
+
+})();
