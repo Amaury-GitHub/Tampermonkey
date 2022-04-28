dfd49460bcb2d7b4f4339d6285de0004169db2e9
Amaury <amaury.data@gmail.com>
2022-04-28 22:52:10 +0800

    Update KG-Assistant.js

--- KG-Assistant.js
+++ KG-Assistant.js
@@ -22,7 +22,6 @@
             document.body.appendChild(document.createElement('script')).src = 'https://cdn.jsdelivr.net/gh/cameroncondry/cbc-kitten-scientists@master/kitten-scientists.user.js';
             document.body.appendChild(document.createElement('script')).src = 'https://cdn.jsdelivr.net/gh/Bioniclegenius/NummonCalc@master/NummonCalc.js';
             document.body.appendChild(document.createElement('script')).src = 'https://cdn.jsdelivr.net/gh/Amaury-GitHub/kittensgame@main/KGP-new.js';
-            document.body.appendChild(document.createElement('script')).src = 'https://cdn.jsdelivr.net/gh/Amaury-GitHub/kittensgame@main/KG-maxValue.js';
         }
     };
     setup();
