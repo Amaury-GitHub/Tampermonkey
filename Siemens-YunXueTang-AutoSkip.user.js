de9b0b847b9421b9cb8d9bcabbddd198ab67864b
Amaury <amaury.data@gmail.com>
2023-03-18 11:16:51 +0800

    Update Siemens-YunXueTang-AutoSkip.user.js

--- Siemens-YunXueTang-AutoSkip.user.js
+++ Siemens-YunXueTang-AutoSkip.user.js
@@ -4,16 +4,26 @@
 // @version      1.0
 // @description  西门子云学堂自动点击继续学习
 // @author       Amaury
-// @include     *siemens.yunxuetang.cn/*
+// @match        *://siemens.yunxuetang.cn/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
+    var isVisible = function(element) {
+      return element.offsetWidth > 0 || element.offsetHeight > 0;
+}
+
     setInterval(function() {
-      const buttonEle = document.getElementsByClassName('yxt-button yxt-button--primary')[2];
-      if (buttonEle) buttonEle.click();
+      const buttons = document.getElementsByClassName('yxt-button yxt-button--primary');
+      for (let i = 0; i < buttons.length; i++) {
+          const buttonText = buttons[i].textContent;
+          if (buttonText === "继续学习" && isVisible(buttons[i])) {
+              buttons[i].click();
+              console.log("Button " + i + " with text '" + buttonText + "' was clicked!");
+          }
+      }
     }, 5000)
 
 })();
