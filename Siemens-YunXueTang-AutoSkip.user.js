d3310fbf6e0953e6d53e643c8e6088dd7a2b7bc7
Amaury <amaury.data@gmail.com>
2023-03-18 10:01:47 +0800

    Create Siemens-YunXueTang-AutoSkip.user.js

---
+++ Siemens-YunXueTang-AutoSkip.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Siemens YunXueTang AutoSkip
+// @namespace    https://github.com/Amaury-GitHub/Tampermonkey
+// @version      1.0
+// @description  西门子云学堂自动点击继续学习
+// @author       Amaury
+// @include     *siemens.yunxuetang.cn/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    setInterval(function() {
+      const buttonEle = document.getElementsByClassName('yxt-button yxt-button--primary')[2];
+      if (buttonEle) buttonEle.click();
+    }, 5000)
+  
+})();
