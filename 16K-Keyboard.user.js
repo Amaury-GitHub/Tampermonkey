fca70256bf3b3b542dab5a62816736d998d5d995
Amaury <amaury.data@gmail.com>
2023-09-25 09:37:30 +0800

    Update 16K-Keyboard.user.js

--- 16K-Keyboard.user.js
+++ 16K-Keyboard.user.js
@@ -9,23 +9,24 @@
 // ==/UserScript==
 
 (function() {
-	document.onkeydown = function(event) {
-		if (event.keyCode == 37) {
-			// ←
-			document.getElementsByClassName('btn btn-outline-secondary')[0].click()
-		}
-		if (event.keyCode == 70) {
-			// F
-			document.getElementsByClassName('btn btn-outline-secondary')[1].click()
-		}
-		if (event.keyCode == 39) {
-			// →
-			document.getElementsByClassName('btn btn-outline-secondary')[2].click()
-		}
-	}
+    document.onkeydown = function(event) {
+        if (event.keyCode == 37) {
+            // ←
+            document.getElementsByClassName('btn btn-outline-secondary')[0].click()
+        }
+        if (event.keyCode == 70) {
+            // F
+            document.getElementsByClassName('btn btn-outline-secondary')[1].click()
+        }
+        if (event.keyCode == 39) {
+            // →
+            document.getElementsByClassName('btn btn-outline-secondary')[2].click()
+        }
+    }
     document.addEventListener('mousedown', function(event) {
         if (event.button === 1) {
             // 鼠标中键
+            event.preventDefault()
             document.getElementsByClassName('btn btn-outline-secondary')[2].click()
         }
     });
