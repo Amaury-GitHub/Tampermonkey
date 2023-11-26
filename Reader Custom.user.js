4847c1cac01f0b8800202bce7623665fedbf267f
Amaury <amaury.data@gmail.com>
2023-11-26 23:28:25 +0800

    Create Reader Custom.user.js

---
+++ Reader Custom.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Reader Custom
+// @namespace    https://github.com/Amaury-GitHub/Tampermonkey
+// @version      0.1
+// @description  屏蔽左右键，自动重设尺寸
+// @author       Amaury
+// @match        *://reader.amaury.eu.org/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var initialWidth = 100;
+    var initialHeight = 1000;
+
+    document.onselectstart = function () { return false; }
+    document.oncontextmenu = function () { return false; }
+    window.resizeTo(initialWidth, initialHeight);
+    window.addEventListener('resize', function(event) {
+        window.resizeTo(initialWidth, initialHeight);
+    });
+
+})();
