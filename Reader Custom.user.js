a1b03d465d6e6e660f98feed4dea91dbf65b6b42
Amaury <amaury.data@gmail.com>
2023-11-26 23:19:59 +0800

    Update Reader Custom.user.js

--- Reader Custom.user.js
+++ Reader Custom.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  屏蔽左右键，自动重设尺寸
 // @author       Amaury
-// @match        https://nas.amaury.eu.org:3333/
+// @match        *://book.amaury.eu.org/*
 // @grant        none
 // ==/UserScript==
 
