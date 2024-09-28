9670fa7b429ab8d9ef8822ae0ec4db469ba42e6e
Amaury <amaury.data@gmail.com>
2024-09-28 20:45:04 +0800

    Update and rename KG-maxValue.user.js to unlimitedCatnip.user.js

---
+++ unlimitedCatnip.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Kittens Game unlimited catnip
+// @namespace    https://github.com/Amaury-GitHub/Tampermonkey
+// @version      1.0
+// @description  猫国建设者无限猫薄荷
+// @author       Amaury
+// @include     *kittensgame.com/web/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    //无限猫薄荷, 1s补充一次
+    setInterval(function() {
+        if (game.resPool.resourceMap.catnip.value < game.resPool.resourceMap.catnip.maxValue){
+            game.resPool.resourceMap.catnip.value = game.resPool.resourceMap.catnip.maxValue
+        }
+    }, 1000)
+
+})();
