32a7f3dab1eba8d4361356b0f5c09134580d3758
Amaury <amaury.data@gmail.com>
2024-10-01 00:37:19 +0800

    Update KG-maxValue.user.js

--- KG-maxValue.user.js
+++ KG-maxValue.user.js
@@ -13,46 +13,52 @@
 (function() {
     'use strict';
 
-    //木材
+    //伐木工-木材
     setInterval(function() {
         if (game.resPool.resourceMap.wood.value < game.resPool.resourceMap.wood.maxValue){
             game.resPool.resourceMap.wood.value = game.resPool.resourceMap.wood.maxValue
         }
     }, 2000)
-    //猫薄荷
+    //农民-猫薄荷
     setInterval(function() {
         if (game.resPool.resourceMap.catnip.value < game.resPool.resourceMap.catnip.maxValue){
             game.resPool.resourceMap.catnip.value = game.resPool.resourceMap.catnip.maxValue
         }
     }, 2000)
-    //科学
+    //学者-科学
     setInterval(function() {
         if (game.resPool.resourceMap.science.value < game.resPool.resourceMap.science.maxValue){
             game.resPool.resourceMap.science.value = game.resPool.resourceMap.science.maxValue
         }
     }, 2000)
-    //喵力
+    //猎人-喵力
     setInterval(function() {
         if (game.resPool.resourceMap.manpower.value < game.resPool.resourceMap.manpower.maxValue){
             game.resPool.resourceMap.manpower.value = game.resPool.resourceMap.manpower.maxValue
         }
     }, 2000)
-    //矿物
+    //矿工-矿物
     setInterval(function() {
         if (game.resPool.resourceMap.minerals.value < game.resPool.resourceMap.minerals.maxValue){
             game.resPool.resourceMap.minerals.value = game.resPool.resourceMap.minerals.maxValue
         }
     }, 2000)
-    //信仰
+    //牧师-信仰
     setInterval(function() {
         if (game.resPool.resourceMap.faith.value < game.resPool.resourceMap.faith.maxValue){
             game.resPool.resourceMap.faith.value = game.resPool.resourceMap.faith.maxValue
         }
     }, 2000)
-    //煤
+    //地质学家-煤
     setInterval(function() {
         if (game.resPool.resourceMap.coal.value < game.resPool.resourceMap.coal.maxValue){
             game.resPool.resourceMap.coal.value = game.resPool.resourceMap.coal.maxValue
+        }
+    }, 2000)
+    //地质学家-黄金
+    setInterval(function() {
+        if (game.resPool.resourceMap.gold.value < game.resPool.resourceMap.gold.maxValue){
+            game.resPool.resourceMap.gold.value = game.resPool.resourceMap.gold.maxValue
         }
     }, 2000)
 
